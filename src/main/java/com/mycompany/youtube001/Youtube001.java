9855558bc0545359d0eb040e249815b51cda408d
josej <josej@TABLET-0P3NLKVK>
2024-02-25 12:19:09 -0600

    ejempo de java para el profe alfonso

---
+++ src/main/java/com/mycompany/youtube001/Youtube001.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.youtube001;
+
+/**
+ *
+ * @author josej
+ */
+public class Youtube001 {
+
+    public static void main(String[] args) {
+         Usuario usu1 = new Usuario("joseandres");
+        video vid1= new Video ("gatooo",24000"http;//joseandres");
+        usu1.SubirVideo(vid1);
+        usu1.MostrarInfoUsuario();
+       
+    }
+}
